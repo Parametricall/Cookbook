b8c9fbc3a2cfd3baa8f062028a305707529e8302
Parametricall <22248105+Parametricall@users.noreply.github.com>
2019-09-12 21:02:57 +1000

    Added abilityt oadd new recipes

--- recipes/views/create_recipe.py
+++ recipes/views/create_recipe.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from django.views import View
 
 from ..models import Recipe
@@ -7,6 +9,12 @@ from ..forms import CreateRecipeForm
 
 class CreateRecipe(View):
     template_name = "recipes/create_recipe.html"
+
+    def post(self, request):
+        form = CreateRecipeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('recipes:Landing Page'))
 
     def get(self, request):
         form = CreateRecipeForm()
