9395a6b4bb1902bb09c091cbcb150983d2bd913f
Parametricall <22248105+Parametricall@users.noreply.github.com>
2019-09-12 21:32:33 +1000

    WIP.. improving details layout

--- recipes/views/recipe_details.py
+++ recipes/views/recipe_details.py
@@ -8,20 +8,26 @@ from ..forms import CreateRecipeForm
 class RecipeDetails(View):
     template_name = "recipes/recipe_details.html"
 
-    def post(self, request, recipe_id):
-        recipe = Recipe.objects.get(id=recipe_id)
-        form = CreateRecipeForm(recipe)
-
-        context = {
-            'form': form,
-        }
-        return render(request, self.template_name, context)
+    # def post(self, request, recipe_id):
+    #     recipe = Recipe.objects.get(id=recipe_id)
+    #     form = CreateRecipeForm(recipe)
+    #
+    #     context = {
+    #         'form': form,
+    #     }
+    #     return render(request, self.template_name, context)
 
     def get(self, request, recipe_id):
         recipe = Recipe.objects.get(id=recipe_id)
         form = CreateRecipeForm(instance=recipe)
 
+        ingredients = recipe.ingredients.split(';')
+        method = recipe.method.split(';')
+
+
         context = {
             'form': form,
+            'ingredients': ingredients,
+            'method': method,
         }
         return render(request, self.template_name, context)
