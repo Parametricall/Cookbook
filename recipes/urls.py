b43bbfa4441a73547a4be04956cc8fc50b2f51cb
Parametricall <22248105+Parametricall@users.noreply.github.com>
2019-09-12 20:49:32 +1000

    Recipe details page created

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from .views.landing_page import LandingPage
 from .views.create_recipe import CreateRecipe
+from .views.recipe_details import RecipeDetails
 
 app_name = "recipes"
 urlpatterns = [
     path('', LandingPage.as_view(), name="Landing Page"),
     path("create_recipe/", CreateRecipe.as_view(), name="Create Recipe"),
+    path('recipe_details/<int:recipe_id>/', RecipeDetails.as_view(), name="Recipe Details"),
 ]
