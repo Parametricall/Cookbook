3daaa4182d341ba8063dbe5f7e1329fc72055887
Parametricall <22248105+Parametricall@users.noreply.github.com>
2019-12-23 00:39:38 +1000

    1154757853090875: Cleaned up code to remove legacy crap.
    Task Summary: Re-create the create_recipe view using React

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,11 +3,10 @@ from django.urls import path
 from .views.landing_page import LandingPage
 from .views.create_recipe import CreateRecipe
 from .views.recipe_details import RecipeDetails
-from .views.react_test_view import ReactTestView
+
 app_name = "recipes"
 urlpatterns = [
     path('', LandingPage.as_view(), name="Landing Page"),
     path("create_recipe/", CreateRecipe.as_view(), name="Create Recipe"),
     path('recipe_details/<int:recipe_id>/', RecipeDetails.as_view(), name="Recipe Details"),
-    path('react', ReactTestView.as_view(), name='React Test View'),
 ]
