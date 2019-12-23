072454727958f73364b56620a91e2fa92a883499
Parametricall <22248105+Parametricall@users.noreply.github.com>
2019-12-23 13:42:50 +1000

    1154757853090875: Updated project for production delpoyment test.
    Task Summary: Re-create the create_recipe view using React

---
+++ frontend/webpack.common.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BundleTracker = require('webpack-bundle-tracker');
+
+
+module.exports = {
+    context: __dirname,
+
+    entry: './src/create_recipe.js',
+    output: {
+        path: path.resolve('./static/assets/bundles/'),
+        filename: '[name]-[hash].js',
+    },
+
+    module: {
+        rules: [
+            {test: /\.(js)$/, use: 'babel-loader'},
+            {test: /\.(css)$/, use: ['style-loader', 'css-loader']}
+        ]
+    },
+
+    mode: 'development',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'templates/frontend/index.html'
+        }),
+        new BundleTracker({filename: './webpack-stats.json'})
+    ]
+};
+
