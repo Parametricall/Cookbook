82d2179e013108fabf2e3b90b626fd396578ee79
Parametricall <22248105+Parametricall@users.noreply.github.com>
2020-01-07 21:28:02 +1000

    Updated package.json to use npm run watch
    Removed webpack-stats.json

--- frontend/webpack.dev.js
+++ frontend/webpack.dev.js
@@ -1,7 +1,12 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const path = require('path');
 
 module.exports = merge(common, {
+    output: {
+        path: path.resolve('./static/assets/bundles/'),
+        filename: '[name]-[hash].js',
+    },
     mode: 'development',
     devtool: 'inline-source-map',
     devServer: {
